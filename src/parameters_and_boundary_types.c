3d11c1dde29720792fe59b82e559d7e88ae94625
Derek <harrison.derek.w@gmail.com>
2020-09-22 00:17:03 +0200

    add document describing algorithm and update kershaw algo

--- src/parameters_and_boundary_types.c
+++ src/parameters_and_boundary_types.c
@@ -28,30 +28,30 @@ void set_parameters_and_boundary_types(domain_size_t* domain_size,
      */
 
     /* Set parameters and boundary conditions */
-    domain_size->Lx = 0.5;                               //length of domain along x coordinate
-    domain_size->Ly = 0.5;                               //length of domain along y coordinate
+    domain_size->Lx = 1.0;                               //length of domain along x coordinate
+    domain_size->Ly = 1.0;                               //length of domain along y coordinate
     domain_size->Lz = 1.0;                               //length of domain along z coordinate
 
-    grid_size->nx = 21;                                   //amount of nodes along x coordinate
-    grid_size->ny = 19;                                   //amount of nodes along y coordinate
-    grid_size->nz = 39;                                   //amount of nodes along z coordinate
+    grid_size->nx = 40;                                   //amount of nodes along x coordinate
+    grid_size->ny = 40;                                   //amount of nodes along y coordinate
+    grid_size->nz = 40;                                   //amount of nodes along z coordinate
 
     time_data->timesteps = 100;                          //number of timesteps
     time_data->ti        = 0.0;                          //initial time
-    time_data->tf        = 0.01;                          //final time
+    time_data->tf        = 0.02;                          //final time
     time_data->Tinitial  = 0.0;                         //inital temperature of system
 
-    physical_parameters->conductivity.gammax = 15.1;     //conductivity along x coordinate
-    physical_parameters->conductivity.gammay = 11.1;     //conductivity along y coordinate
-    physical_parameters->conductivity.gammaz = 7.1;     //conductivity along z coordinate
-    physical_parameters->Cp                  = 10.0;     //heat capacity
-    physical_parameters->rho                 = 10.0;      //density
+    physical_parameters->conductivity.gammax = 1.0;     //conductivity along x coordinate
+    physical_parameters->conductivity.gammay = 1.0;     //conductivity along y coordinate
+    physical_parameters->conductivity.gammaz = 1.0;     //conductivity along z coordinate
+    physical_parameters->Cp                  = 1.0;     //heat capacity
+    physical_parameters->rho                 = 1.0;      //density
 
     boundary_type_faces->west_boundary   = DIRICHLET;    //west face boundary type
-    boundary_type_faces->east_boundary   = NEUMANN;    //east face boundary type
+    boundary_type_faces->east_boundary   = DIRICHLET;    //east face boundary type
     boundary_type_faces->south_boundary  = DIRICHLET;    //bottom face boundary type
-    boundary_type_faces->north_boundary  = NEUMANN;    //north face boundary type
-    boundary_type_faces->bottom_boundary = NEUMANN;    //bottom face boundary type
+    boundary_type_faces->north_boundary  = DIRICHLET;    //north face boundary type
+    boundary_type_faces->bottom_boundary = DIRICHLET;    //bottom face boundary type
     boundary_type_faces->top_boundary    = DIRICHLET;    //top face boundary type
 
     *exportData = TRUE;                                  //export data guard
